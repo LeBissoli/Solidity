75778eb127fa9b4372be372d35c8ce609ff4b086
LeBissoli <84162785+LeBissoli@users.noreply.github.com>
2021-05-30 21:53:11 -0300

    Create Rifa.sol

---
+++ Rifa.sol
@@ -0,0 +1,33 @@
+/*SPDX-License-Identifier: CC-BY-4.0
+(c) Desenvolvido por Leandro Bissoli na aula do Jeff Prestes
+This work is licensed under a Creative Commons Attribution 4.0 International License.
+Projeto para criar um smart contract para execução de Rifas ;)
+*/
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Rifa{
+    
+    // Estruturar uma Rifa
+    
+    // Estruturar o Participante - Nome, Carteira, Numero da Sorte 
+    
+    // Criar Rifa - Informar Nome
+    
+    // Adicionar Participante - Informar o Nome, Carteira
+    
+    // Verificar Eligibilidade do Participante - Regras de Rifa 
+    
+    // Encerrar Participacao na Rifa - Informar o Número da Rifa
+    
+    // Identificar Ganhador - Informar o número da Rifa e selecionar aleatóriamente o ganhador
+    
+    
+    
+    ///// Back-Office:
+    // Listar Participantes 
+    // Detalhes da Promoção
+    // Detalhes do Participante
+    
+    
+}
