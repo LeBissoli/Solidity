e3ca6ddee2408e405c4c7b5438e857f5aa235ef8
LeBissoli <84162785+LeBissoli@users.noreply.github.com>
2021-06-24 21:46:21 -0300

    Update PartidaInteface.sol

--- PartidaInteface.sol
+++ PartidaInteface.sol
@@ -11,23 +11,81 @@ import  './OraclePartidas.sol';
 
 contract PartidaInteface {
     
-  /* struct partida {
+   struct aposta {
+        address payable carteiraApostador;
+        uint idAposta;
+        uint valorAposta;
         uint dataRegistro;
-        string time1_Casa;
-        string time2_Visitante;
-        uint timeVencedor;
-        bool resultadoAtualizado;
-    }*/
-
+    }
+    
+    // Remete para o contrato de partidas e resultados
     OraclePartidas internal contratoPartidas;
     
+    mapping(uint => aposta[]) public listaApostas;
     
     constructor (address _enderecoContrato){
         contratoPartidas =  OraclePartidas(_enderecoContrato);
         
     }
+    
+    function registrarAposta(uint _idPartida, uint _idAposta) public payable {
+        
+       // address payable enderecoCarteiraApostador = ; // apenas a carteira 
+       // uint valorAposta = msg.value; // O valor de aposta do Participante
+        
+        // Cadastro a aposta
+        aposta memory apostaNova;               // Criacao de nova aposta
+        apostaNova.carteiraApostador        = payable(msg.sender);
+        apostaNova.idAposta                 = _idAposta;
+        apostaNova.valorAposta              = msg.value;
+        apostaNova.dataRegistro             = block.timestamp;
 
-    function retornarPartida(uint _idPartida) public view returns (uint _idTimeVencedor, string memory _time1_Casa, string memory _time2_Visitante){
+        // REGISTRA A APOSTA
+        listaApostas[_idPartida].push(apostaNova);
+        
+    }
+    
+    
+    function listarApostas(uint _idPartida) public view returns (aposta[] memory){
+        return listaApostas[_idPartida];
+    }
+    
+    function valorTotalApostasPartida(uint _idPartida) public view returns (uint[] memory _valorApostasPartida){
+        
+        uint length = listaApostas[_idPartida].length;
+        uint[] memory valorApostasPartida;
+
+        for(uint i = 0; i < length; i++) {
+            valorApostasPartida[listaApostas[_idPartida][i].idAposta] += listaApostas[_idPartida][i].valorAposta;
+        } 
+        return valorApostasPartida;
+    }
+    
+    function valorTotalCustodia() public view returns(uint){
+        return address(this).balance;
+    }
+    
+    function valorTotalApostas() public view returns (uint[] memory _valorTotalApostas){
+        
+        // Pegar o total de Partidas no OraclePartidas
+        uint totalPartidas = contratoPartidas.listarQuantidadePartidas();
+        uint[] memory valorTotalApostas;
+        
+        for(uint idPartida = 0; idPartida < totalPartidas; idPartida++) {
+            uint totalApostas = listaApostas[idPartida].length;
+            
+            for(uint numAposta = 0; numAposta < totalApostas; numAposta++) {
+                valorTotalApostas[listaApostas[idPartida][numAposta].idAposta] += listaApostas[idPartida][numAposta].valorAposta;
+            }
+        }
+        
+        return valorTotalApostas;
+        
+    }
+    
+    
+    
+    /*function retornarPartida(uint _idPartida) public view returns (uint _idTimeVencedor, string memory _time1_Casa, string memory _time2_Visitante){
         uint idTimeVencedor;
         string memory time1_Casa;
         string memory time2_Visitante;
@@ -36,5 +94,7 @@ contract PartidaInteface {
         (idTimeVencedor, time1_Casa, time2_Visitante) = contratoPartidas.retornarPartida(_idPartida);
        
         return (idTimeVencedor, time1_Casa, time2_Visitante);
-    }
+    }*/
+    
+    
 }
